80eaefb45e04e63113ccb1d94340093e025d986e
fahmi-ramadhan <fahmi122004@gmail.com>
2023-10-21 21:08:13 +0700

    refactor forward function

--- Forward.m
+++ Forward.m
@@ -1,7 +1,9 @@
 function [ x ] = Forward( L,b )
-  n=length(b);
-  x=zeros(n,1);
-  for i=1:n
-    x(i)=(b(i)-L(i,1:i-1)*x(1:i-1))/L(i,i);
+  n = length(b);
+  x = zeros(n,1);
+  x(1) = b(1)/L(1,1);
+  for i=2:n
+    sum = L(i,1:i-1) * x(1:i-1);
+    x(i) = (b(i)-sum) / L(i,i);
   endfor
-end
+endfunction
