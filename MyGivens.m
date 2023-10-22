50f1dda31ec5ab6399f8f4955b2fd02e120d40c0
fahmi-ramadhan <fahmi122004@gmail.com>
2023-10-22 16:10:22 +0700

    fixing bugs

--- MyGivens.m
+++ MyGivens.m
@@ -1,15 +1,15 @@
 function [ R,bt ] = MyGivens ( A,b )
-  [m,n]=size(A);
-  A=[A b];
-  for k=1:n
-    for j=k+1:n
-      r=sqrt(A(k,k)^2+A(k,j)^2);
-      c=A(k,k)/r;
-      s=A(k,j)/r;
-      temp = c*A(k,k:n+1) + s*A(j,k:n+1);
-      A(j,k:n+1) = -s*A(k,k:n+1) + c*A(j,k:n+1);
-      A(k,k:n+1) = temp;
+  [m,n] = size(A);
+  A = [A b];
+  for i = 1:n
+    for j = i+1:m
+      r = sqrt(A(i,i)^2 + A(j,i)^2);
+      c = A(i,i)/r;
+      s = A(j,i)/r;
+      temp = c*A(i,i:n+1) + s*A(j,i:n+1);
+      A(j,i:n+1) = -s*A(i,i:n+1) + c*A(j,i:n+1);
+      A(i,i:n+1) = temp;
     endfor
   endfor
   R=A(:,1:n); bt=A(:,n+1);
-end
+endfunction
